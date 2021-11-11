398c730588c277eaca3ca377be99a5b7f4e04083
Marantha Rodrigues <marantharodrigues@Maranthas-MacBook-Air.local>
2021-11-11 02:06:07 -0500

    Add CustomData Json file

--- FrontEndApp/App.js
+++ FrontEndApp/App.js
@@ -1,26 +1,34 @@
 import React from "react";
 import {DataTable} from 'react-native-paper';
+import customData from './customData.json';
 
 export default function App() {
+  let foodArray = customData;
+  // fetch("https://cors-anywhere.herokuapp.com/https://johnnypark.ca/api/imagelist").then(response => {
+  //   return response.json()
+  //   }).then(json => {
+  //     console.log(json)
+  //     this.setState({
+  //       loading: false,
+  //       fetchedData: json
+  //     })
+  //   }){customData.recipeCriteria1.ingredients}
+
+  
   return (
     <DataTable>
     <DataTable.Header>
-      <DataTable.Title
-        sortDirection='descending'>Item Name</DataTable.Title>
-      <DataTable.Title numeric>Quantity</DataTable.Title>
-      <DataTable.Title numeric>Expiry Date</DataTable.Title>
+    <DataTable.Title>ID</DataTable.Title>
+      <DataTable.Title >Ingredients</DataTable.Title>
+      <DataTable.Title >Health</DataTable.Title>
+      <DataTable.Title >Cuisine Type</DataTable.Title>
+      <DataTable.Title> Meal </DataTable.Title>
     </DataTable.Header>
-    <DataTable.Row>
-        <DataTable.Cell>Frozen yogurt</DataTable.Cell>
-        <DataTable.Cell numeric>159</DataTable.Cell>
-        <DataTable.Cell numeric>Oct 15th</DataTable.Cell>
-      </DataTable.Row>
-
       <DataTable.Row>
-        <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
+        <DataTable.Cell>{customData.food1.ingredients}|</DataTable.Cell>
         <DataTable.Cell numeric>237</DataTable.Cell>
         <DataTable.Cell numeric>Oct 15th</DataTable.Cell>
-      </DataTable.Row>
+      </DataTable.Row> 
 
     </DataTable>
   );
