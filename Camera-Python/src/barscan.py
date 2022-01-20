cceadf15a70348ffe355607baaa8ac4d427beacd
Cameron-chung <cameronchung2@gmail.com>
2022-01-20 13:36:35 -0500

    update to new code

---
+++ Camera-Python/src/barscan.py
@@ -0,0 +1,33 @@
+from pyzbar.pyzbar import decode
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+# initialize the camera and grab a reference to the raw camera capture
+camera = Picamera()
+cap = cv2.VideoCapture(0)
+camera.resolution = (640, 480)
+camera.framerate = 60
+camera.vflip = True
+cap.set(3, 640)
+cap.set(4, 480)
+used_codes = []
+
+camera = True
+while camera == True:
+    success, frame = cap.read()
+    
+    for code in decode(frame):
+        if code.data.decode('utf-8') not in used_codes:
+            print('Approved. You can enter!')
+            print(code.data.decode('utf-8'))
+            used_codes.append(code.data.decode('utf-8'))
+            time.sleep(5)
+        elif code.data.decode('utf-8') in used_codes:
+            print('Sorry, this code has been already used')
+            time.sleep(5)
+        else:
+            pass
+        
+    cv2.imshow('Testing-code-scan', frame)
+    cv2.waitKey(1)
---
+++ Camera-Python/src/test.py
@@ -0,0 +1,51 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+# allow the camera to warmup
+time.sleep(0.1)
+def BarcodeReader(image):
+
+    # read the image in numpy array using cv2
+    img = cv2.imread(image)
+      
+    # Decode the barcode image
+    detectedBarcodes = decode(img)
+      
+    # If not detected then print the message
+    if not detectedBarcodes:
+        print("Barcode Not Detected or your barcode is blank/corrupted!")
+    else:
+          # Traverse through all the detected barcodes in image
+        for barcode in detectedBarcodes: 
+           
+            # Locate the barcode position in image
+            (x, y, w, h) = barcode.rect
+             
+             
+            if barcode.data!="":
+               
+                # Print the barcode data
+                # print(barcode.data)
+                # print(barcode.type)
+                return barcode
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
+	BarcodeReader(image)
+	# show the frame
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
